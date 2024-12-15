1614cea43b951022b7743b17e12bb881b4d89040
pythaac <pythaac.gmail.com>
2024-12-15 12:52:40 +0900

    CategoryController 추가

--- src/main/java/com/bankrupt/bankruptapi/controller/CategoryController.java
+++ src/main/java/com/bankrupt/bankruptapi/controller/CategoryController.java
@@ -1,10 +1,7 @@
 package com.bankrupt.bankruptapi.controller;
 
 import com.bankrupt.bankruptapi.dao.Category;
-import com.bankrupt.bankruptapi.dao.CategoryResource;
-import com.bankrupt.bankruptapi.service.CategoryResourceService;
 import com.bankrupt.bankruptapi.service.CategoryService;
-import com.bankrupt.bankruptapi.service.ScourtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController(value = "/v1/category")
 public class CategoryController {
     private final CategoryService categoryService;
-    private final ScourtService scourtService;
-    private final CategoryResourceService categoryResourceService;
 
     @PostMapping
     public void createCategory(@RequestBody Category category) {
@@ -28,29 +23,5 @@ public class CategoryController {
     @DeleteMapping(value = "/{categoryId}")
     public void deleteCategory(@PathVariable Long categoryId) {
         categoryService.deleteCategory(categoryId);
-    }
-
-    @GetMapping(value = "/relation")
-    public String test() {
-        scourtService.updateDiff();
-        return "done";
-    }
-
-    @PostMapping(value = "/resource")
-    public void createCategoryResource(@RequestBody CategoryResource categoryResource) {
-        categoryResourceService.saveCategoryResource(categoryResource);
-    }
-
-    @PutMapping(value = "/resource/{categoryResourceId}")
-    public void updateCategoryResource(
-            @PathVariable int categoryResourceId,
-            @RequestBody CategoryResource categoryResource) {
-
-    }
-
-    @GetMapping(value = "/row")
-    public String test2() {
-        scourtService.updateDiff();
-        return "done";
     }
 }
