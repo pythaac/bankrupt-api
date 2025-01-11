60f98c195c2e67fe1a74944bf2dbbe6b94bfef23
pythaac <pythaac@gmail.com>
2025-01-11 19:03:32 +0900

    GET /v1/category 추가

--- src/main/java/com/bankrupt/bankruptapi/controller/CategoryController.java
+++ src/main/java/com/bankrupt/bankruptapi/controller/CategoryController.java
@@ -5,10 +5,17 @@ import com.bankrupt.bankruptapi.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController(value = "/v1/category")
 public class CategoryController {
     private final CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> findAllCategories() {
+        return categoryService.findAllCategory();
+    }
 
     @PostMapping
     public void createCategory(@RequestBody Category category) {
