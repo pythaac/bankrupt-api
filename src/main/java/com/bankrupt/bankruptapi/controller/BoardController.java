6fce4d1aaa2bf80f1478e6f36f0fd52e56db0973
pythaac <pythaac@gmail.com>
2025-01-11 18:08:19 +0900

    GET /v1/board에 Pageable 추가

--- src/main/java/com/bankrupt/bankruptapi/controller/BoardController.java
+++ src/main/java/com/bankrupt/bankruptapi/controller/BoardController.java
@@ -2,11 +2,12 @@ package com.bankrupt.bankruptapi.controller;
 
 import com.bankrupt.bankruptapi.dao.Board;
 import com.bankrupt.bankruptapi.service.BoardService;
-import com.bankrupt.bankruptapi.service.ScourtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
@@ -16,8 +17,15 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping
-    public List<Board> getAllBoard() {
-        return boardService.findAllBoardList();
+    public List<Board> getAllBoard(
+            @RequestParam(required = false, defaultValue = "1") Integer page,
+            @RequestParam(required = false, defaultValue = "5") Integer size,
+            @RequestParam(required = false, defaultValue = "uploaded")
+                @Pattern(regexp = "uploaded|index|seller|title|due|category") String sort,
+            @RequestParam(required = false, defaultValue = "desc")
+                @Pattern(regexp = "asc|desc") String direction
+        ) {
+        return boardService.findAllBoardList(page, size, sort, direction);
     }
 
     @GetMapping(value = "/{categoryId}")
