1685a4b3cfbe83a60cbdbb64e3eb6338fa8425d5
pythaac <pythaac@gmail.com>
2025-01-16 21:14:58 +0900

    boardDto 생성 및 board에 모든 category 정보 및 fileLink 추가

---
+++ src/main/java/com/bankrupt/bankruptapi/dto/BoardDto.java
@@ -0,0 +1,46 @@
+package com.bankrupt.bankruptapi.dto;
+
+import com.bankrupt.bankruptapi.dao.Board;
+import com.bankrupt.bankruptapi.dao.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class BoardDto {
+    private Long id;
+    private String seller;
+    private String court;
+    private String title;
+    private String uploaded;
+    private String due;
+    private String fileLink;
+    private String telephoneNumber;
+    private List<CategoryDto> categories;
+
+    private static String fileUrl = "https://file.scourt.go.kr/AttachDownload?path=011";
+
+    public static BoardDto of(Board board, List<Category> categories) {
+        return BoardDto.builder()
+                .id(board.getId())
+                .seller(board.getSeller())
+                .court(board.getCourt())
+                .title(board.getTitle())
+                .uploaded(board.getUploaded())
+                .due(board.getDue())
+                .telephoneNumber(board.getTelephoneNumber())
+                .fileLink(getFileLink(board.getFile(), board.getFileName()))
+                .categories(categories.stream().map(CategoryDto::of).toList())
+                .build();
+    }
+
+    private static String getFileLink(String file, String fileName) {
+        return fileUrl + "&file=" + file + "&downFile=" + fileName;
+    }
+}
--- src/main/java/com/bankrupt/bankruptapi/dto/CategoryDto.java
+++ src/main/java/com/bankrupt/bankruptapi/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.bankrupt.bankruptapi.dto;
 
+import com.bankrupt.bankruptapi.dao.Category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,13 @@ import lombok.NoArgsConstructor;
 @Builder
 @Data
 public class CategoryDto {
+    private Long id;
     private String categoryName;
+
+    public static CategoryDto of(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .categoryName(category.getCategoryName())
+                .build();
+    }
 }
