ee60aca0765df25df90de9757d645f351e1840c1
pythaac <pythaac@gmail.com>
2025-01-21 22:01:05 +0900

    sync를 1시간에 1번씩 동작하도록 설정

--- src/main/java/com/bankrupt/bankruptapi/service/SyncService.java
+++ src/main/java/com/bankrupt/bankruptapi/service/SyncService.java
@@ -2,6 +2,7 @@ package com.bankrupt.bankruptapi.service;
 
 import com.bankrupt.bankruptapi.repository.BoardRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,8 @@ public class SyncService {
         return boardRepository.findLatestUpdated();
     }
 
+    // 1000ms * 60 * 60 = 1hour
+    @Scheduled(fixedDelay = 3_600_000)
     public void syncScourt() {
 
         if (!syncTask.isDone()) {
