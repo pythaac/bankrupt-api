42b54fa7cb04031210ab9fdd724a6c00437cacbe
pythaac <pythaac@gmail.com>
2025-02-02 17:01:16 +0900

    ScourtBoardClient를 SourtClient로 이름 변경

---
+++ src/main/java/com/bankrupt/bankruptapi/feign/ScourtClient.java
@@ -0,0 +1,27 @@
+package com.bankrupt.bankruptapi.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+import static com.bankrupt.bankruptapi.Constant.SCORUT_URL;
+
+@FeignClient(value = "scourtClient", url = SCORUT_URL)
+public interface ScourtClient {
+
+    @PostMapping("/portal/notice/realestate/RealNoticeList.work")
+    String getScourtBoardPage(
+            @RequestHeader Map<String, Object> header
+            , @RequestParam("pageSize") Integer pageSize
+            , @RequestParam("pageIndex") Integer pageIndex
+    );
+
+    @PostMapping("/portal/notice/realestate/RealNoticeView.work")
+    String getScourtBoardDetailPage(
+            @RequestHeader Map<String, Object> header
+            , @RequestParam("seq_id") Long seq_id
+    );
+}
