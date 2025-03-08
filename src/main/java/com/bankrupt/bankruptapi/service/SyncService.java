1a7db345ea7f67d8506e8285b17ae34e782b41da
pythaac <pythaac@gmail.com>
2025-03-08 23:47:04 +0900

    syncEnabled 추가 및 config에 추가

--- src/main/java/com/bankrupt/bankruptapi/service/SyncService.java
+++ src/main/java/com/bankrupt/bankruptapi/service/SyncService.java
@@ -1,35 +1,40 @@
 package com.bankrupt.bankruptapi.service;
 
-import com.bankrupt.bankruptapi.repository.BoardRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ConcurrentModificationException;
 import java.util.concurrent.CompletableFuture;
 
 @Service
-@AllArgsConstructor
 public class SyncService {
-    private final BoardRepository boardRepository;
     private final ScourtService scourtService;
 
     private static LocalDateTime lastSyncTime;
     private static CompletableFuture<Void> syncTask = CompletableFuture.completedFuture(null);
 
+    @Value("${sync.enabled:true}")
+    private boolean syncEnabled;
+
+    public SyncService(ScourtService scourtService) {
+        this.scourtService = scourtService;
+    }
+
     public LocalDateTime getSyncTime() {
         return lastSyncTime;
     }
 
-    @Scheduled(fixedDelayString = "${sync.scheduled}")
+    @Scheduled(fixedDelayString = "${sync.fixed-delay}")
     public void syncScourt() {
-        lastSyncTime = LocalDateTime.now();
-
+        if (!syncEnabled) {
+            return;
+        }
         if (!syncTask.isDone()) {
             throw new ConcurrentModificationException();
         }
+        lastSyncTime = LocalDateTime.now();
         syncTask = CompletableFuture.runAsync(scourtService::updateDiff);
     }
 }
